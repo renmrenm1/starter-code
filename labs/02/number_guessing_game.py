229d88a3913cad68d24d782d3c0e966a523a7e10
renmrenm1 <rampageser1@gmail.com>
2026-09-18 20:49:53 -0700

    lab

--- labs/02/number_guessing_game.py
+++ labs/02/number_guessing_game.py
@@ -4,9 +4,11 @@ num = random.randint(1, 100)
 user = input("Enter your guess: ")
 user = int(user)
 
-if user > num:
-    print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
+while
+    if user > num:
+        print("Too high, it was", num)
+    elif user < num:
+        print("Too low, it was", num)
+    else:
+        print("You got it!")
+        print("The number was", num)
